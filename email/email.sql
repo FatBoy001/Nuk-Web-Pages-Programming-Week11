718d54001711e41da3b1f09a9998256c6fe99d3e
FatBoy001 <51392131+FatBoy001@users.noreply.github.com>
2022-05-09 23:44:41 +0800

    Add files via upload

---
+++ email/email.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2022-05-09 17:39:16
+-- 伺服器版本： 10.4.22-MariaDB
+-- PHP 版本： 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫: `email`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `email`
+--
+
+CREATE TABLE `email` (
+  `no` int(11) NOT NULL,
+  `userEmail` varchar(50) COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- 傾印資料表的資料 `email`
+--
+
+INSERT INTO `email` (`no`, `userEmail`) VALUES
+(5, 'w@q'),
+(6, 'aas19895@gmail.com'),
+(7, 'a1083365@mail.nuk.edu.tw');
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `email`
+--
+ALTER TABLE `email`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `email`
+--
+ALTER TABLE `email`
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
